da20e0fb4cc3df09419246f7c2a84d13f1b10b41
felipearc13 <41086151+felipearc13@users.noreply.github.com>
2020-08-25 20:10:39 -0300

    Update start-termux-f13.sh

--- start-termux-f13.sh
+++ start-termux-f13.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 #Instalando repositorios
+pkg update -y && pkg upgrade -y
 pkg install  root-repo unstable-repo x11-repo -y
-pkg update -y && apt upgrade -y
+pkg update -y && pkg upgrade -y
 
 #Instalando OpenSSH
+pkg update -y && pkg upgrade -y
 pkg install openssh termux-auth -y
 
 #Instalando FTP
+pkg update -y && pkg upgrade -y
 pkg install busybox termux-services -y 
 source $PREFIX/etc/profile.d/start-services.sh
 
@@ -16,6 +19,7 @@ sv-enable ftpd
 sv up ftpd
 
 #Instalando tor e torsocks
+pkg update -y && pkg upgrade -y
 pkg install tor torsocks -y
 mv $PREFIX/etc/tor/torrc $PREFIX/etc/tor/torrc.bkp
 wget https://raw.githubusercontent.com/felipearc13/termux-ini-f13/master/torrc -P $PREFIX/etc/tor/
@@ -24,22 +28,34 @@ mkdir ~/doc/
 wget https://raw.githubusercontent.com/felipearc13/termux-ini-f13/master/info-tor.md -P ~/doc/
 
 #Adicionando extra-keys
+pkg update -y && pkg upgrade -y
 pkg install git raw -y
 mkdir ~/.termux
 wget https://raw.githubusercontent.com/felipearc13/termux-ini-f13/master/termux.properties -P ~/.termux/
+
+#Instalando vim
+pkg update -y && pkg upgrade -y
+pkg insttall vim -y
+
+#Instalando Ubuntu
+pkg update -y && pkg upgrade -y
+pkg install proot proot-distro -y 
+proot-distro install ubuntu
+
+#Instalando zsh
+pkg update -y && pkg upgrade -y
+pkg install zsh -y 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
 
 #Modificando motd
 mv /data/data/com.termux/files/usr/etc/motd  /data/data/com.termux/files/usr/etc/motd.bkp
 wget https://raw.githubusercontent.com/felipearc13/termux-ini-f13/master/motd -P /data/data/com.termux/files/usr/etc/
 
-#Instalando vim
-pkg insttall vim -y
 
-#Instalando Ubuntu
-pkg install proot proot-distro -y 
-proot-distro install ubuntu
 
 #Graphical Environment
+pkg update -y && pkg upgrade -y
 pkg install tigervnc openbox pypanel xorg-xsetroot xfce4-terminal aterm st fltk megatools openbox obconf xterm xfce4-settings polybar libnl geanypcmanfm rofi feh neofetch curl pulseaudio elinks dosbox -y
 vncserver
 123456
@@ -52,14 +68,6 @@ chmod +x ~/.vnc/xstartup
 mv $PREFIX/etc/xdg/openbox/autostart $PREFIX/etc/xdg/openbox/autostart.bkp
 wget https://raw.githubusercontent.com/felipearc13/termux-ini-f13/master/autostart -P $PREFIX/etc/xdg/openbox/
 chmod +x $PREFIX/etc/xdg/openbox/autostart
-
-#Instalando zsh
-pkg install zsh -y 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-#Editando sources.list
-mv  $PREFIX/etc/apt/sources.list  $PREFIX/etc/apt/sources.list.bkp
-wget https://raw.githubusercontent.com/felipearc13/termux-ini-f13/master/sources.list -P $PREFIX/etc/apt/
 
 #Acessa memoria do celular
 termux-setup-storage
